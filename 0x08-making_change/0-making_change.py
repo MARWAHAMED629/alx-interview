a2a72a31dcae1488b778949d5f3f1293ceb6cf79
root <mmero385@gmail.com>
2024-08-22 23:02:18 +0000

    making_change ✔️

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Change making module.
+"""
+
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+
+    # Create an array to store the minimum number of coins required to reach each value
+    m_coins = [float('inf')] * (total + 1)
+    m_coins[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            # Update the minimum number of coins required for each value
+            m_coins[i] = min(m_coins[i], m_coins[i - coin] + 1)
+
+    # Return the minimum number of coins required to reach the total value
+    return m_coins[total] if m_coins[total] != float('inf') else -1
