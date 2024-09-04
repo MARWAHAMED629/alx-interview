e43ad8fa4b52f990778399634f5d4ab873acea53
root <mmero385@gmail.com>
2024-09-03 18:26:42 -0700

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
-def isWinner(x, nums):
-    if x <= 0 or not nums or x != len(nums):
+"""0. Prime Game - Maria and Ben are playing a game"""
+
+
+def isWinner(rounds, numbers):
+
+    if rounds <= 0 or numbers is None:
+        return None
+    if rounds != len(numbers):
         return None
 
-    max_num = max(nums)
-    primes = [1] * (max_num + 1)
-    primes[0] = primes[1] = 0
+    ben_score = 0
+    maria_score = 0
 
-    for i in range(2, int(max_num**0.5) + 1):
-        if primes[i]:
-            primes[i*i:max_num+1:i] = [0] * len(primes[i*i:max_num+1:i])
+    primes_list = [1 for _ in range(sorted(numbers)[-1] + 1)]
+    primes_list[0], primes_list[1] = 0, 0
+    for index in range(2, len(primes_list)):
+        remove_multiples(primes_list, index)
 
-    maria_wins = sum(sum(primes[:n+1]) % 2 != 0 for n in nums)
-    ben_wins = x - maria_wins
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    if ben_wins > maria_wins:
+    for num in numbers:
+        if sum(primes_list[0:num + 1]) % 2 == 0:
+            ben_score += 1
+        else:
+            maria_score += 1
+    if ben_score > maria_score:
         return "Ben"
+    if maria_score > ben_score:
+        return "Maria"
     return None
+
+
+def remove_multiples(primes_array, factor):
+
+    for index in range(2, len(primes_array)):
+        try:
+            primes_array[index * factor] = 0
+        except (ValueError, IndexError):
+            break
